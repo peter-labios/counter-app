244d8feb7b27d90ab27f1c1dc7d678e4796833d3
unknown <peter.labios@gmail.com>
2022-08-25 11:22:50 +0800

    increment working

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import {createStore} from 'redux';
 
-const reducerFn = (state = {counter: 10}, action) => {
+const reducerFunction = (state = {counter: 0}, action) => {
+    if(action.type==="INC"){
+        return {counter: state.counter+1}
+    }
+
     return state;
 }
 
-const store = createStore(reducerFn);
+const store = createStore(reducerFunction);
 export default store;
