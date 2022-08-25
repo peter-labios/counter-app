af5f97f3a5d7147927f63e65fe54346e4a69d65f
unknown <peter.labios@gmail.com>
2022-08-25 11:29:16 +0800

    added increment by 10

--- src/App.js
+++ src/App.js
@@ -13,12 +13,17 @@ function App() {
     dispatch({type: 'DEC'})
   }
 
+  const addValue = () => {
+    dispatch({type: 'ADD', payload:10})
+  }
+
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={addValue}>Add by 10 </button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import {createStore} from 'redux';
 
 const reducerFunction = (state = {counter: 0}, action) => {
     if(action.type==="INC"){
-        return {counter: state.counter+1}
+        return {
+            counter: state.counter+1
+        }
     }
     if(action.type==="DEC"){
-        return {counter: state.counter-1}
+        return {
+            counter: state.counter-1
+        }
+    }
+    if(action.type==="ADD"){
+        return {
+            counter: state.counter + action.payload
+        }
     }
 
     return state;
